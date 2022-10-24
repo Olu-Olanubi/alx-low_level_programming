1ac45c8ef0203d107033221d5d7543fa49343ebb
Olu-Olanubi <seyeolanubi@gmail.com>
2022-10-24 13:23:26 -0700

    function that updates the value of variable through its pointer

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,6 +9,6 @@ int _putchar(char c);
  * reset_to_98 - updates the value in a pointer to 98
 * @n: input- pointer to an int variable 
 */
-void reset_to_98(int *n)
+void reset_to_98(int *n);
 
 #endif
