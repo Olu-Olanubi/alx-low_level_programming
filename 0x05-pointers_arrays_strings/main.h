e2e216bf2766f346cd541800858c6b024d911d38
Olu-Olanubi <seyeolanubi@gmail.com>
2022-10-24 14:44:24 -0700

    Function to print a string in reverse

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -30,4 +30,9 @@ int _strlen(char *s);
 
  */
 void _puts(char *str);
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: string to reverse
+ */
+void print_rev(char *s);
 #endif
