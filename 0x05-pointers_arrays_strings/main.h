fb29d39271085b355631bbf19f880f8ee1cf17c8
Olu-Olanubi <seyeolanubi@gmail.com>
2022-10-24 13:14:03 -0700

    function to update a value through a pointer

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+*/
+int _putchar(char c);
+/**
+ * reset_to_98 - updates the value in a pointer to 98
+* @n: input- pointer to an int variable 
+*/
+void reset_to_98(int *n)
+
+#endif
