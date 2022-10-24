fbb896ddf0d4ee41b6597d3ce79d08ca7c17486c
Olu-Olanubi <seyeolanubi@gmail.com>
2022-10-24 16:58:14 -0700

    Function to print the last half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - prints a string, in reverse, followed by a new line
+ * @str: string to print half
+ */
+void puts_half(char *str)
+{
+int i, half;
+i = 0;
+while (str[i] != '\0')
+{
+i++;
+}
+if (i % 2 == 0)
+half = i / 2;
+else
+half = (i -1) / 2;
+while (half <= i - 1)
+{
+_putchar(str[half]);
+half++;
+}
+_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -47,4 +47,10 @@ void rev_string(char *s);
  * Return: void
  */
 void puts2(char *str);
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ * Return: void
+ */
+void puts_half(char *str);
 #endif
