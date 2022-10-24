5f478bd2bfac13480ecf2d7354353e83cdd5fcd2
Olu-Olanubi <seyeolanubi@gmail.com>
2022-10-24 13:38:23 -0700

    Function to swap integers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,5 +10,10 @@ int _putchar(char c);
 * @n: input- pointer to an int variable 
 */
 void reset_to_98(int *n);
-
+/**
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+void swap_int(int *a, int *b);
 #endif
