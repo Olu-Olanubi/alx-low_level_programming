c1c2ebde29f02e9c1fe05ef251ae1e5e47f46c9b
Olu-Olanubi <seyeolanubi@gmail.com>
2022-10-27 20:36:58 +0100

    ROT 13 function

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,16 @@
+#include "main.h"
+char *rot13(char *s)
+{
+char input[53] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+char output[53] = {'N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m'};
+int i, j;
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        for (j = 0; input[j]!= '\0' && s[i] != input[j]; j++)
+        {
+        }
+        if (s[i] == input[j])
+            s[i] = output[j];
+    }
+   return (s);
+}
