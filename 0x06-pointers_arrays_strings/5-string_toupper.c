42c9d136caa0661ee528272ce89d8091d1addb7a
Olu-Olanubi <seyeolanubi@gmail.com>
2022-10-26 17:26:18 -0700

    Function to convert a string to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase of string to uppercase
+ * @s: string array
+ * Return: string array
+ */
+char *string_toupper(char *c)
+{
+int i;
+for (i = 0; c[i] != '\0'; i++)
+{
+if (c[i] >= 'a'  && (c[i] < 'z'))
+c[i] = c[i] - 32;
+}
+return (c);
+}
