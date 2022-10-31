b2b36d8ab24af884fbbfd5daf115c31fe3e8274f
Olu-Olanubi <seyeolanubi@gmail.com>
2022-11-01 00:12:31 +0100

    C function to locate a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#define NULL 0
+/**
+ *_memcpy - locates a character in a string
+ * @s: string to be searched through
+ * @c: character to be searched for
+ * Return: pointer to the first occurrence of c in s or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+int i  = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+return (s + i);
+}
+return (NULL);
+}
