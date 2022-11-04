904925f8d02cbb8b4985321e21cb107683d28549
Olu-Olanubi <seyeolanubi@gmail.com>
2022-11-04 23:17:21 +0100

    Function that prints its own mame

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,9 @@
+/**
+ * main - print name of program
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
 #include "main.h"
 #include <stdio.h>
 int main(int argc, char *argv[])
