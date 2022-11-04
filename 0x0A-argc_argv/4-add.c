fc86c19c9e35aacb7ee20bec433ae51925dd6245
Olu-Olanubi <seyeolanubi@gmail.com>
2022-11-05 00:50:49 +0100

    Function adds the positive integers parsed to it

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the sum of arguments it receives
+ * @argc: argument counter
+ * @argv: pointer to array of arguments with numbers to multiply included
+ * Return: 0 on success, 1 if error
+ */
+int main(int argc, char *argv[])
+{
+int sum = 0;
+int i;
+for (i = 1; i < argc; i++)
+{
+/** If one of the number contains symbols that are not digits,
+* print Error, followed by a new line, and return 1
+*/
+if (*argv[i] < '0' || *argv[i] > '9')
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+sum += atoi(argv[i]);
+}
+}
+printf("%d\n", sum);
+return (0);
+}
