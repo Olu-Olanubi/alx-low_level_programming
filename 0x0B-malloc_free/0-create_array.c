102022539e55c923bceac7f3382c46402793cf6e
Olu-Olanubi <seyeolanubi@gmail.com>
2022-11-08 00:11:49 +0100

    Function that creates and initializes an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,6 +9,7 @@
 char *create_array(unsigned int size, char c)
 {
 char *arr;
+unsigned int i;
 if (size <= 0)
 {
 return (NULL);
@@ -18,10 +19,9 @@ if (arr == NULL)
 {
 return (NULL);
 }
-unsigned int i;
 for (i = 0; i < size; i++)
 {
-*(arr + i) == c;
+*(arr + i) = c;
 }
 return (arr);
 }
