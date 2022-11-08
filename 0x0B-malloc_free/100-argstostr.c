8491496af6ef1f3c497d16f38a808f1ca751eaa5
Olu-Olanubi <seyeolanubi@gmail.com>
2022-11-08 02:49:19 +0100

    Function to concatenate args

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,15 +3,15 @@
 int _strlen(char *s);
 /**
  * argstostr - concatenates all the arguments.
- * @grid: grid to be freed 
- * @height: height of array
+ * @ac: count of arguments
+ * @av: pointer to array of arguments
  * Return: pointer to 2D array
  */
 char *argstostr(int ac, char **av)
 {
 char *s;
 int length = 0, i, j, k = 0;
-if(ac == 0 || av == NULL)
+if (ac == 0 || av == NULL)
 return (NULL);
 for (i = 0; i < ac; i++)
 {
@@ -19,7 +19,7 @@ length += _strlen(av[i]);
 }
 length += (ac + 1); /* add space for newlines and null terminator */
 /* allocate memory and free if error */
-s = malloc(length * sizeof(char));
+s = malloc(length *sizeof(char));
 if (s == NULL)
 {
 free(s);
