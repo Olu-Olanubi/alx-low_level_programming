b6e3d89bc1d67e759102fc4f5e7aaa04a9976b3f
Olu-Olanubi <seyeolanubi@gmail.com>
2022-11-12 00:30:50 +0100

    First commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-malloc_checked.c -o a
+./a
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+Custom C functions based on dynamic memory allocation function "malloc"
