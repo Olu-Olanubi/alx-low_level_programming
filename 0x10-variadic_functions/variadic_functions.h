1b3262e848d0e7dcb4b2ae7875a5305d682122d0
Olu-Olanubi <seyeolanubi@gmail.com>
2022-11-17 17:30:30 +0100

    Included function that prints strings

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,6 +6,9 @@
  * Return: int
 */
 int _putchar(char c);
+#include <stdarg.h>
+#include <stdio.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 #endif
