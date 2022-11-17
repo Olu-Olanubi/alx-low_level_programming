4defaba35abd6781f4595800eaff7078a472a747
Olu-Olanubi <seyeolanubi@gmail.com>
2022-11-17 01:21:42 +0100

    Modified function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 /**
  * _putchar - puts character to standard output
  * @c: character to put to standard output
+ * Return: int
 */
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
