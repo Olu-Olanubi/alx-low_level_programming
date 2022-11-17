76016e20c86028e23e6735fcfb8d85934ea94ffb
Olu-Olanubi <seyeolanubi@gmail.com>
2022-11-17 01:43:44 +0100

    Included file function array_iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * array_iterator - executes function on each parameter of the array
+ * @array: array of integers
+ * @size: Size of array
+ * @action: function to be executed
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+if (array == NULL || action == NULL)
+return;
+i = 0;
+while(i < size)
+{
+action(array[i]);
+i++;
+}
+}
---
+++ 0x0F-function_pointers/1-array_iterator.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-array_iterator.c -o b
+./b
