fbade9aeb493953024ec8b9c6807ced550b059ee
Olu-Olanubi <seyeolanubi@gmail.com>
2022-11-17 17:02:36 +0100

    First commit, Function that sums all its argument

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ * Return: int
+*/
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+#endif
