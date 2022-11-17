14c7d7c7a6186abcedf5b35af3deb1f873084bda
Olu-Olanubi <seyeolanubi@gmail.com>
2022-11-17 02:04:27 +0100

    Included 2-int_index.c which contains function that searches or an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * int_index - function that searches or an integer
+ * @array: array of integers
+ * @size: Size of array
+ * @cmp: comparison function
+ * Return:  index of the first element for which the cmp function does not return 0
+ * If no element matches, return -1
+ * If size <= 0, return -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (size <= 0)
+return (-1);
+if (array == NULL || cmp == NULL)
+return (-1);
+i = 0;
+while (i < size)
+{
+if (cmp(array[i]) != 0)
+return (i);
+i++;
+}
+return (-1);
+}
---
+++ 0x0F-function_pointers/2-int_index.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-int_index.c -o c
+./c
