da6f7b90f85f46c0bb16b052a779fd0b353c75ba
Olu-Olanubi <seyeolanubi@gmail.com>
2022-11-25 01:20:54 +0100

    Function that executes the main() function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**before_main - print before the main function
+ * Return: void
+ */
+void before_main(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon ");
+printf("my back!\n");
+}
---
+++ 0x12-singly_linked_lists/100-main.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 100-main.c 100-first.c -o first
+./first
