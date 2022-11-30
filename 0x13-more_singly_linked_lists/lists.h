6eceecd8e97c421b80d9138ac3fdffcf74d13e74
Olu-Olanubi <seyeolanubi@gmail.com>
2022-11-30 04:43:24 +0100

    Function adds a new node at the beginning of a list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -38,4 +38,5 @@ void free_list(list_t *head);
 void __attribute__((constructor)) before_main(void);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 #endif
