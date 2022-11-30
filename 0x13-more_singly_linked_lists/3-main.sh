9ff2a73cc73bf81b9ab0fb6a098434c23601ccd2
Olu-Olanubi <seyeolanubi@gmail.com>
2022-11-30 04:49:15 +0100

    Function adds a new node at the end of a list

---
+++ 0x13-more_singly_linked_lists/3-main.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-add_nodeint_end.c 0-print_listint.c -o d
+./d
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -39,4 +39,5 @@ void __attribute__((constructor)) before_main(void);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 #endif
