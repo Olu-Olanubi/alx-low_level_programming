0b55a9d503aeff205b584f9baabff42c27c51c0f
Olu-Olanubi <seyeolanubi@gmail.com>
2022-12-02 02:03:43 +0100

    Function prints a binary as integer

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,6 +3,7 @@
 /**
  * _putchar - puts character to standard output
  * @c: character to put to standard output
+ *  Return: On success 1, -1 if error
 */
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
