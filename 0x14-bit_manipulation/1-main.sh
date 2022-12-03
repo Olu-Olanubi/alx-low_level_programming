601dbc4be61dd6a0358587a0e5ccc69c9cec8d92
Olu-Olanubi <seyeolanubi@gmail.com>
2022-12-03 10:35:42 +0100

    Function that prints a binary representation of a number

---
+++ 0x14-bit_manipulation/1-main.sh
@@ -0,0 +1,2 @@
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-print_binary.c _putchar.c -o b
+./b
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,5 +6,7 @@
  *  Return: On success 1, -1 if error
 */
 int _putchar(char c);
+void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 #endif
